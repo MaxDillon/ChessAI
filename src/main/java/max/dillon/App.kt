81166a4e9229ef508dcc76b239966f050568dea0
bendill <bendill@nuevaschool.org>
2018-03-14 14:54:33 -0700

    added some stuff

--- src/main/java/max/dillon/App.kt
+++ src/main/java/max/dillon/App.kt
@@ -9,11 +9,36 @@ import java.nio.file.Paths
 
 
 fun printArray(anArray: Array<Array<Int>>) {
-//    println(Arrays.deepToString(anArray))
-    anArray.forEach { thing ->
-        println(Arrays.deepToString(thing))
+    anArray.forEach {
+        it.forEach {
+            val c = it.toString()
+            if (it>=0) print(" ")
+            print(c)
+            print("  ")
+        }
+        println("\n")
     }
 }
+
+fun getPieceMoves(x: Int, y: Int): Int {
+
+
+    return "".hashCode()
+}
+
+
+fun getBoardStates(board: Array<Array<Int>>) {
+    val possibleStates = arrayListOf<Int>()
+    board.forEachIndexed { x, list->
+        list.forEachIndexed { y, piece ->
+            if(piece != 0) possibleStates.add(getPieceMoves(x,y))
+
+        }
+    }
+
+
+}
+
 
 fun main(args: Array<String>) {
     val str = String(Files.readAllBytes(Paths.get("src/main/data/chess.textproto")))
@@ -36,16 +61,17 @@ fun main(args: Array<String>) {
             if (gameBoard[coord[1]-1][coord[0]-1]==0) {
 
                 gameBoard[coord[1]-1][coord[0]-1] = index+1
-                gameBoard[game.boardSize-coord[1]][game.boardSize-coord[0]] = index+1
+                gameBoard[game.boardSize-coord[1]][game.boardSize-coord[0]] = -index-1
 
             } else throw InvalidStateException("there is already a piece at $coord")
         }
     }
 
-    println("board:")
+    println("\nboard:\n")
 
     printArray(gameBoard)
-
+    getBoardStates(gameBoard)
 
 }
+
 
