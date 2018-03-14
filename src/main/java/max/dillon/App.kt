1a77cecaaf7afbfa19e98690d13aeb297328fa61
bendill <bendill@nuevaschool.org>
2018-03-14 13:57:46 -0700

    placement list

--- src/main/java/max/dillon/App.kt
+++ src/main/java/max/dillon/App.kt
@@ -1,11 +1,20 @@
 package max.dillon
 
+
+import java.util.Arrays;
 import com.google.protobuf.TextFormat
-import java.io.IOException
+import sun.plugin.dom.exception.InvalidStateException
 import java.nio.file.Files
 import java.nio.file.Paths
+import kotlin.math.absoluteValue
 
 
+fun printArray(anArray: Array<Array<Int>>) {
+//    println(Arrays.deepToString(anArray))
+    anArray.forEach { thing ->
+        println(Arrays.deepToString(thing))
+    }
+}
 
 fun main(args: Array<String>) {
     val str = String(Files.readAllBytes(Paths.get("src/main/data/chess.textproto")))
@@ -22,19 +31,24 @@ fun main(args: Array<String>) {
     }
 
 
-
-
     val gameBoard = Array( game.boardSize,{ Array(game.boardSize,{0}) } )
 
 
     game.pieceList.forEach { piece ->
         piece.placementList.forEach { placement ->
             val coord = placement.substring(1).split("y").map { it.toInt() }
-            if (gameBoard[coord[0]][coord[1]]==0) {
+            println("${piece.name} $coord ")
 
-            }
+            if (gameBoard[coord[0]-1][coord[1]-1]==0) {
+
+                gameBoard[coord[0]-1][coord[1]-1] = piece.hashCode().absoluteValue%8
+                gameBoard[game.boardSize-coord[0]][game.boardSize-coord[1]] = piece.hashCode().absoluteValue%8
+
+            } else throw InvalidStateException("there is already a piece at $coord")
         }
     }
+    printArray(gameBoard)
+
 
 }
 
