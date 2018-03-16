b34b2234d0467bb2618bc3bfe3b4af1115f582a5
maxdillon <maxdillon13124@gmail.com>
2018-03-15 22:35:17 -0700

    Progress on initialization and legal move determination

---
+++ src/main/java/max/dillon/App.java
@@ -0,0 +1,23 @@
+package max.dillon;
+
+import com.google.protobuf.TextFormat;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class App {
+    public static void main(String[] args) throws IOException {
+        String str = new String(Files.readAllBytes(Paths.get("src/main/data/chess.textproto")));
+        GameGrammar.GameSpec.Builder builder = GameGrammar.GameSpec.newBuilder();
+        TextFormat.getParser().merge(str, builder);
+        GameGrammar.GameSpec game = builder.build();
+
+        System.out.println(game.getName());
+        System.out.println(game.getBoardSize());
+        for (GameGrammar.Piece piece : game.getPieceList()) {
+            System.out.println(piece.getName() + " " + piece.getPlacementList());
+        }
+    }
+}
+
