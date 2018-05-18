7816c28f25d32a051f9a14d10df21cd6fa8b7576
bendill <bendill@nuevaschool.org>
2018-05-17 17:06:30 -0700

    tried to do gui stuff

--- src/main/java/maximum/industries/AppServer.kt
+++ src/main/java/maximum/industries/AppServer.kt
@@ -28,7 +28,7 @@ fun main(args: Array<String>) {
     val server = embeddedServer(Netty, port = 8080) {
         var white = getAlgo("mcts",SearchParameters(1,1.0,1.0,1))
         var black = getAlgo("mcts",SearchParameters(1,1.0,1.0,1))
-        var playerTurnWhite = true
+        var playerWhite = true
 
         install(ContentNegotiation) {
             gson {
@@ -39,32 +39,28 @@ fun main(args: Array<String>) {
 
 
             post("/start") {
-                playerTurnWhite = call.receive<Boolean>()
+                playerWhite = call.receive<Boolean>()
                 state = GameState(gameSpec)
-                val whiteAlgo = if (playerTurnWhite) "gui" else "mcts"
-                white = getAlgo(whiteAlgo)
+                val whiteAlgo = if (playerWhite) "gui" else "amcts"
+                white = getAlgo(whiteAlgo, SearchParameters(5000))
 
-                val blackAlgo = if (playerTurnWhite) "mcts" else "gui".also { state = white.next(state).first}
-                black = getAlgo(blackAlgo)
-
+                val blackAlgo = if (playerWhite) "amcts" else "gui"
+                black = getAlgo(blackAlgo,SearchParameters(5000))
                 call.respond(Pair(state.toWireState(),gameSpec))
+            }
+
+            get("/opponentMove") {
+                state = if (playerWhite) black.next(state).first else white.next(state).first
+                call.respond(state.toWireState())
             }
 
 
             post("/move") {
                 val received = call.receive<WireState>()
-                val sig = received.state.hashCode()
+                val color = if (playerWhite) white else black
                 if (true) {
-                    if (playerTurnWhite) {
-                        white.index(received.moveIndex)
-                        state = white.next(received.toGameState(gameSpec)).first
-                        state = black.next(state).first
-                    }
-                    else {
-                        black.index(received.moveIndex)
-                        state = black.next(received.toGameState(gameSpec)).first
-                        state = white.next(state).first
-                    }
+                    color.index(received.moveIndex)
+                    state = color.next(received.toGameState(gameSpec)).first
 
                     call.respond(state.toWireState())
 
@@ -74,7 +70,7 @@ fun main(args: Array<String>) {
 
             }
 
-            static ("/") {
+            static {
                 default("static/webpage.html")
                 files("static")
             }
@@ -87,42 +83,11 @@ fun main(args: Array<String>) {
 data class ObjectState (val board: ByteArray,
                   val moves: IntArray,
                   val whiteMove: Boolean,
-                  val moveDepth: Int) {
-    override fun hashCode(): Int {
-        val modulo = 7529350
-        val secretKey = 58327403923465493
-        val boardHash = board.contentToString().hashCode()
-        return ((secretKey*boardHash*whiteMove.hashCode() * moveDepth.hashCode())%modulo).toInt()
-    }
-    override fun equals(other: Any?): Boolean {
-        if (other !is ObjectState){
-            return false
-        } else return board.contentEquals(other.board) &&
-                moves.contentEquals(other.moves) &&
-                whiteMove == other.whiteMove &&
-                moveDepth == other.moveDepth
-
-    }
-}
+                  val moveDepth: Int)
 
 data class WireState(val state: ObjectState,
                      val moveIndex: Int,
-                     val signature: Int) {
-
-    override fun hashCode(): Int {
-        val modulo = 78621349
-        val secretKey = 58327403923465493 % modulo
-        return ((secretKey * state.hashCode())%modulo).toInt()
-    }
-    override fun equals(other: Any?): Boolean {
-        if (other !is WireState) {
-            return false
-        } else return state.equals(other.state)
-    }
-
-}
-
-
+                     val signature: Int)
 
 class GuiInput : GameSearchAlgo {
     var moveIndex = 0
