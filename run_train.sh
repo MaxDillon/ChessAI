0888e3b24d0208d1efe0982ba74e919994f58e0b
bendill <bendill@nuevaschool.org>
2018-06-03 14:08:47 -0700

    Generate data with deeper search

--- run_train.sh
+++ run_train.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
+lead=0
+skip=0
+
+for arg in "$@"; do
+    case $arg in
+        -lead)  lead=1; shift;;
+        -skip)  skip=1; shift;;
+    esac
+done
+
 while true; do
-    ./mi_play.sh chess2 \
-        -n 10 \
-        -white "model1:model.chess2.Model010.*" \
-        -witer 250 -wexpl 0.2 -wtemp 0.1 \
-        -black "model1:model.chess2.Model010.*" \
-        -biter 250 -bexpl 0.2 -btemp 0.1 \
-        > /dev/null
+    if [[ $skip == 0 ]]; then
+        ./mi_play.sh chess2 \
+            -n 10 \
+            -white "model1:model.chess2.Model010.*" \
+            -witer 600 -wexpl 0.2 -wtemp 0.1 \
+            -black "model1:model.chess2.Model010.*" \
+            -biter 600 -bexpl 0.2 -btemp 0.1 \
+            > /dev/null
+    fi
     
-    if [[ "$1" == "lead" ]]; then
+    if [[ $lead != 0 ]]; then
         ./fetch.sh
         ./reshuffle.sh
         ./mi_train.sh chess2 \
@@ -17,8 +29,8 @@ while true; do
             -from $(ls -t model.chess2.Model010.* | head -n 1) \
             -valuemult 0.95 \
             -metf 0.7 \
-            -lastn 15 \
-            -batch 500 \
+            -lastn 30 \
+            -batch 1200 \
             -drawweight 0.2 \
             -saveevery 1000 \
             -updates 1
