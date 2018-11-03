cd95bcfea7193c35e2a4b42c201190995cbe9d2d
Tom Dillon <tom.dillon@gmail.com>
2018-11-02 19:01:12 -0700

    New prod model

--- run_train.sh
+++ run_train.sh
@@ -4,6 +4,7 @@ lead=0
 skip=0
 fast=0
 dest=/dev/null
+device=0
 
 for arg in "$@"; do
     case $arg in
@@ -11,6 +12,7 @@ for arg in "$@"; do
         -skip)  skip=1; shift;;
         -fast)  fast=1; shift;;
         -show)  dest=/dev/stdout; shift;;
+        -dev1)  device=1; shift;;
     esac
 done
 
@@ -23,6 +25,7 @@ while true; do
                 -witer 400 -wexpl 0.15 -wtemp 0.3 -wpexp 2.0 -wunif 1.0 \
                 -black "model2:model.chess2.Model013.*" \
                 -biter 400 -bexpl 0.15 -btemp 0.3 -bpexp 2.0 -bunif 1.0 \
+		-device $device \
                 > $dest
         else
             ./mi_play.sh chess2 \
@@ -35,6 +38,7 @@ while true; do
                 -fast true \
                 -mindepth 30 \
                 -rollback 6 \
+		-device $device \
                 > $dest
         fi
     fi
@@ -53,5 +57,9 @@ while true; do
             -saveevery 1000 \
             -updates 1 \
             -rate 5e-3
+    fi
+
+    if [ -e exit_run_train ]; then
+	exit
     fi
 done
